5822ffb2086fe2664af33ca5eb65d0c60f75cabc
DeisyMelo <deisymelo0127@gmail.com>
2023-11-17 17:31:51 -0500

    update splash window, implement instructions

---
+++ app/src/main/java/com/example/equipo8/view/Home.kt
@@ -0,0 +1,12 @@
+package com.example.equipo8.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.equipo8.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/example/equipo8/view/MainActivity.kt
+++ app/src/main/java/com/example/equipo8/view/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.equipo8.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.example.equipo8.R
 import com.example.equipo8.databinding.ActivityMainBinding
-import com.example.equipo8.view.fragment.inicio
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,15 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            val fragmentManager = supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            val fragment = inicio()
-            fragmentTransaction.add(R.id.fragment_container, fragment)
-            fragmentTransaction.commit()
-        },5000)
     }
 }
 
