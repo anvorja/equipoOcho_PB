a8710fc463e3bce3c0168de58fef63a9c514f3f6
DeisyMelo <deisymelo0127@gmail.com>
2023-11-17 03:18:00 -0500

    splashWindow in mainActivity

--- app/src/main/java/com/example/equipo8/view/MainActivity.kt
+++ app/src/main/java/com/example/equipo8/view/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.equipo8.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.equipo8.databinding.ActivityMainBinding
+import com.example.equipo8.view.fragment.inicio
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,6 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, inicio::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
     }
 }
 
